988bd754d2fa4b535579f769a7ee6dde36c0fb26
busrauman <busrauman93@gmail.com>
2018-06-08 22:32:42 +0300

    initialize spring boot app Structure

---
+++ src/main/java/com/library/app/main/LibraryAppApplication.java
@@ -0,0 +1,12 @@
+package com.library.app.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAppApplication.class, args);
+	}
+}
